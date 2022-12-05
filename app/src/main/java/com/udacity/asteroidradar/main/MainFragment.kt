f5ee5078a152997ec5ec42647fd377cdca6cd88f
MosaTech <osamaelmesady1@gmail.com>
2022-12-06 00:48:05 +0200

    Initial commit  and final

---
+++ app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
@@ -0,0 +1,87 @@
+package com.udacity.asteroidradar.main
+
+import android.os.Build
+import android.os.Bundle
+import android.view.*
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import com.udacity.asteroidradar.Asteroid
+import com.udacity.asteroidradar.R
+import com.udacity.asteroidradar.databinding.FragmentMainBinding
+import com.udacity.asteroidradar.utils.FilterAsteroid
+
+@RequiresApi(Build.VERSION_CODES.N)
+class MainFragment : Fragment() {
+
+    private val viewModel: MainViewModel by lazy {
+        val activity = requireNotNull(this.activity) {
+            "You can only access the viewModel after onViewCreated()"
+        }
+
+        ViewModelProvider(
+            this,
+            MainViewModel.Factory(activity.application)
+        ).get(MainViewModel::class.java)
+    }
+
+    private val asteroidAdapter = MainAdapter(MainAdapter.AsteroidListener { asteroid ->
+        viewModel.onAsteroidClicked(asteroid)
+    })
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = FragmentMainBinding.inflate(inflater)
+        binding.lifecycleOwner = this
+
+        binding.viewModel = viewModel
+        binding.asteroidRecycler.adapter = asteroidAdapter
+
+        viewModel.navigateToDetailAsteroid.observe(viewLifecycleOwner, Observer { asteroid ->
+            if (asteroid != null) {
+                this.findNavController().navigate(MainFragmentDirections.actionShowDetail(asteroid))
+                viewModel.onAsteroidNavigated()
+            }
+        })
+
+        setHasOptionsMenu(true)
+
+        return binding.root
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.asteroidList.observe(viewLifecycleOwner, Observer<List<Asteroid>> { asteroid ->
+            asteroid.apply {
+                asteroidAdapter.submitList(this)
+            }
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.main_overflow_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        viewModel.onChangeFilter(
+            when (item.itemId) {
+                R.id.show_rent_menu -> {
+                    FilterAsteroid.TODAY
+                }
+                R.id.show_all_menu -> {
+                    FilterAsteroid.WEEK
+                }
+                else -> {
+                    FilterAsteroid.ALL
+                }
+            }
+        )
+        return true
+    }
+}
